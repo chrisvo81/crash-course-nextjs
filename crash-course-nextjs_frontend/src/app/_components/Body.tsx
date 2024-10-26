c127cae1b87e6b5a808092392950056f8ce749f7
Vo, Chris <vochris81@gmail.com>
2024-10-25 22:57:57 -0400

    bug: data issue

--- crash-course-nextjs_frontend/src/app/_components/Body.tsx
+++ crash-course-nextjs_frontend/src/app/_components/Body.tsx
@@ -5,16 +5,22 @@ const Body = async () => {
   const response = await axios.get("http://127.0.0.1:1337/api/camps");
   const data = response.data.data;
 
-  return data ? (
+  return (
     <div>
-      {data.map((camp: any) => (
-        <div key={camp.id}>
-          <h2>{camp.name}</h2>
-          <p>{camp.description}</p>
-        </div>
-      ))}
+      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
     </div>
-  ) : null;
+  );
+
+  // return data ? (
+  //   <div>
+  //     {data.map((camp: any) => (
+  //       <div key={camp.id}>
+  //         <h2>{camp.name}</h2>
+  //         <p>{camp.description}</p>
+  //       </div>
+  //     ))}
+  //   </div>
+  // ) : null;
 };
 
 export default Body;
